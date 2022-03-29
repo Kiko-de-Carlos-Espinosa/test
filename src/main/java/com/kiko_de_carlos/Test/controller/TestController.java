91268afe46f71d031783aa6ed47f301dfc270390
Kiko de Carlos <kiko.de.carlos@concento.es>
2022-03-30 00:19:18 +0200

    Second commit

--- src/main/java/com/kiko_de_carlos/Test/controller/TestController.java
+++ src/main/java/com/kiko_de_carlos/Test/controller/TestController.java
@@ -1,15 +1,25 @@
 package com.kiko_de_carlos.Test.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.kiko_de_carlos.Test.dto.Product;
 
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -21,13 +31,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @RequestMapping("/product")
 public class TestController {
     
-    //private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping("/{productId}/similarids")
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping(value = "/{productId}/similar", produces = { "application/json", "application/xml" })
     @Operation(
-        operationId = "get-product-similarids",
-        summary = "Gets the ids of the similar products",
-        description = "Returns the similar products to a given one ordered by similarity"
+        operationId = "get-product-similar",
+        summary = "Similar products",
     )
     @ApiResponses(value = { 
         @ApiResponse(
@@ -36,37 +47,35 @@ public class TestController {
             content = { 
                 @Content(mediaType = "application/json", 
                 schema = @Schema(
+
                     implementation = String.class)) 
             }
         )
     })
-	public ArrayList<String> listSimilarids(@PathVariable("productId") String productId) {  
-        return null;
+	public ResponseEntity<JSONObject> listSimilarids(@PathVariable("productId") String productId) {  
+        
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity <JSONObject> request = new HttpEntity<>(headers)
+
+            ResponseEntity respon = restTemplate.exchange(
+                "UrlMockup:3001//product/{productId}/similarids",
+                HttpMethod.GET,
+                request,
+                JSONObject.class
+            );
+            return respon;
+        } catch (Exception e) {
+            logger.error("error");
+            return responseErrorEntity(HttpStatus.INTERNAL_SERVER_ERROR, "error");
+        }
     } 
 
-    @GetMapping("/{productId}")
-	@Operation(
-        operationId = "get-product-productId",
-        summary = "Gets a product detail",
-        description = "Returns the product detail for a given productId"
-    )
-    @ApiResponses(value = { 
-        @ApiResponse(
-            responseCode = "200", 
-            description = "Ok", 
-            content = { 
-                @Content(mediaType = "application/json", 
-                schema = @Schema(
-                    implementation = Product.class)) 
-            }
-        ),
-        @ApiResponse(
-            responseCode = "400", 
-            description = "Product Not found"
-        )
-    })
-    public Product getProduct(@PathVariable("productId") String productId) {  
-        return null;
+    private ResponseEntity<JSONObject> responseErrorEntity(final HttpStatus code, final String message) {
+        JSONObject error = new JSONObject();
+        error.put("errorMessage",message);
+        return new ResponseEntity<>(error,code);
     }
 
 }
